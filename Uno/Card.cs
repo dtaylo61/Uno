78cddd18daa2a9c7c35e0c282f835764bbfe1cf4
Donté Taylor <tedon73@gmail.com>
2019-03-29 14:34:02 -0400

    Add project files.

---
+++ Uno/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uno
+{
+    class Card
+    {
+        public string[] colorOfCard = new string[] { "Red", "Green", "Yellow", "Blue" };
+        public string[] typeOfCard = new string[] { "Numerical", "Action" };
+        public int[] numericalCard = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public string[] actionCard = new string[] { "Reverse", "Skip", "Draw Two", "Wild", "Wild Draw Four" };
+
+        public void Reverse()
+        {
+
+        }
+
+        public void Skip()
+        {
+
+        }
+
+        public void DrawTwo()
+        {
+
+        }
+
+        public void Wild()
+        {
+
+        }
+
+        public void WildDrawFour()
+        {
+
+        }
+    }
+}
---
+++ Uno/Game.cs
@@ -0,0 +1,22 @@
+﻿// Uno Card Game
+
+// This is a computer console-based version of one of my favorite card games, Uno. This game consist of classes for a player’s hand, a card, the table, the deck, score, and a game.
+// This program enforces the rules of the game and actually shuffles cards in an array before each deal, thereby making good use of System.Random. In addition, it supports multiple players and never deals the same card twice.
+// In this game, the player is able to play against one or more computer opponents.  Over a period of time, other components may be implemented to make the computer a more challenging opponent by making it “smarter”.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
